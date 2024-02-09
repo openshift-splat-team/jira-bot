5714c4f6e9ec55d7be9b6095bfa4670a7c91d342
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-02-09 11:04:06 -0500

    build epic status updates

---
+++ cmd/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	jira "github.com/andygrunwald/go-jira"
+)
+
+func main() {
+	token := os.Getenv("JIRA_PERSONAL_ACCESS_TOKEN")
+	if len(token) == 0 {
+		fmt.Println("JIRA_PERSONAL_ACCESS_TOKEN must be exported")
+		os.Exit(1)
+	}
+	tp := jira.BearerAuthTransport{
+		Token: token,
+	}
+
+	jiraClient, _ := jira.NewClient(tp.Client(), "https://issues.redhat.com/")
+	issues, _, err := jiraClient.Issue.Search("filter = \"SPLAT Team - Epics 4.16\"", nil)
+	if err != nil {
+		panic(err)
+	}
+	for _, issue := range issues {
+		childIssues, _, err := jiraClient.Issue.Search(fmt.Sprintf("\"Parent Link\" = \"%s\"", issue.Key), nil)
+		if err != nil {
+			fmt.Printf("unable to get child issues for %s: %v", issue.Key, err)
+			continue
+		}
+
+		fmt.Printf("%d child issues\n", len(childIssues))
+		aggregatePoints := 0.0
+		completedPoints := 0.0
+		inprogressPoints := 0.0
+		unsizedStories := 0
+		for _, childIssue := range childIssues {
+			points := childIssue.Fields.StoryPoints
+			aggregatePoints += points
+			if childIssue.Fields.Status.Name == "Closed" {
+				completedPoints += points
+			} else if childIssue.Fields.Status.Name != "Backlog" {
+				inprogressPoints += points
+			}
+			if points == 0 {
+				unsizedStories++
+			}
+		}
+		fmt.Printf("%f total points\n", aggregatePoints)
+
+		propertyMap := map[string]interface{}{
+			"fields": map[string]interface{}{
+				"customfield_12310243": aggregatePoints,
+				"customfield_12320841": fmt.Sprintf("completed: %.0f/%.0f\nin progress: %.0f/%.0f\nunsized stories: %d", completedPoints, aggregatePoints, inprogressPoints, aggregatePoints, unsizedStories),
+			},
+		}
+		resp, err := jiraClient.Issue.UpdateIssue(issue.Key, propertyMap)
+		if err != nil {
+			fmt.Printf("unable to update epic %s: %v\n", issue.Key, err)
+			b := []byte{}
+			resp.Response.Body.Read(b)
+			fmt.Println(string(b))
+			continue
+		}
+	}
+}

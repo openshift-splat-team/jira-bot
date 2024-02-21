c05459d2560673427891d676c137ef80cb44e78b
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-02-20 21:02:32 -0500

    add issue update command

--- cmd/issue/auto-update-issues.go
+++ cmd/issue/auto-update-issues.go
@@ -9,31 +9,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	FieldStoryPoints   = "customfield_12310243"
-	FieldStatusSummary = "customfield_12320841"
-)
-
-type autoUpdateIssueOptions struct {
-	defaultSpikeStoryPoints int64
-	dryRunFlag              bool
-	overrideFlag            bool
-}
-
-func (a *autoUpdateIssueOptions) checkSetSpikePoints(client *jira.Client, issue jira.Issue) error {
+func checkSetSpikePoints(client *jira.Client, issue jira.Issue, options *issueCommandOptions) error {
 	if issue.Fields.Type.Name == "Spike" {
-		if util.GetStoryPoints(issue.Fields.Unknowns) > 0 && !a.overrideFlag {
+		if util.GetStoryPoints(issue.Fields.Unknowns) > 0 && !options.overrideFlag {
 			log.Fatalf("issue: %s already has assigned story points.  run again and provide --override=true to apply", issue.Key)
 			return nil
 		}
 
-		if util.GetStoryPoints(issue.Fields.Unknowns) == 0 || a.overrideFlag {
+		if util.GetStoryPoints(issue.Fields.Unknowns) == 0 || options.overrideFlag {
 			propertyMap := map[string]interface{}{
 				"fields": map[string]interface{}{
-					util.FieldStoryPoints: a.defaultSpikeStoryPoints,
+					util.FieldStoryPoints: options.defaultSpikeStoryPoints,
 				},
 			}
-			if a.dryRunFlag {
+			if options.dryRunFlag {
 				log.Printf("issue: %s would have default spike points assigned. run again and provide --dry-run=false to apply.", issue.Key)
 				return nil
 			} else {
@@ -49,7 +38,7 @@ func (a *autoUpdateIssueOptions) checkSetSpikePoints(client *jira.Client, issue 
 }
 
 // autoUpdateIssuesInQuery according to rules set forth by the team
-func (a *autoUpdateIssueOptions) autoUpdateIssuesInQuery(jql string) error {
+func autoUpdateIssuesInQuery(jql string, options *issueCommandOptions) error {
 	log.Printf("preparing to auto-update issues found in query: %s", jql)
 	jiraClient, err := util.GetJiraClient()
 	if err != nil {
@@ -64,8 +53,8 @@ func (a *autoUpdateIssueOptions) autoUpdateIssuesInQuery(jql string) error {
 	log.Printf("%d issues found in query", len(issues))
 
 	for _, issue := range issues {
-		if a.defaultSpikeStoryPoints > 0 {
-			err = a.checkSetSpikePoints(jiraClient, issue)
+		if options.defaultSpikeStoryPoints > 0 {
+			err = checkSetSpikePoints(jiraClient, issue, options)
 			if err != nil {
 				return fmt.Errorf("unable to set default spike story points: %v", err)
 			}
@@ -74,23 +63,15 @@ func (a *autoUpdateIssueOptions) autoUpdateIssuesInQuery(jql string) error {
 	return nil
 }
 
-func Initialize(rootCmd *cobra.Command) {
-	options := autoUpdateIssueOptions{}
-
-	cmdAutoUpdateIssuesStatus := &cobra.Command{
-		Use:   "auto-update-issues [jql]",
-		Short: "Updates issues according to rules provided as options.",
-		Long:  `Updates issues matching the JQL provided according to rules provided as options`,
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			err := options.autoUpdateIssuesInQuery(args[0])
-			if err != nil {
-				util.RuntimeError(fmt.Errorf("unable to update issues: %v", err))
-			}
-		},
-	}
-	cmdAutoUpdateIssuesStatus.Flags().BoolVarP(&options.dryRunFlag, "dry-run", "d", true, "only apply changes with --dry-run=false")
-	cmdAutoUpdateIssuesStatus.Flags().BoolVarP(&options.overrideFlag, "override", "o", false, "overrides a warning when --override=true")
-	cmdAutoUpdateIssuesStatus.Flags().Int64VarP(&options.defaultSpikeStoryPoints, "default-spike-points", "s", -1, "points to apply to spikes which have no points")
-	rootCmd.AddCommand(cmdAutoUpdateIssuesStatus)
+var cmdAutoUpdateIssuesStatus = &cobra.Command{
+	Use:   "auto-update-issues [jql]",
+	Short: "Updates issues according to rules provided as options.",
+	Long:  `Updates issues matching the JQL provided according to rules provided as options`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		err := autoUpdateIssuesInQuery(args[0], &options)
+		if err != nil {
+			util.RuntimeError(fmt.Errorf("unable to update issues: %v", err))
+		}
+	},
 }
